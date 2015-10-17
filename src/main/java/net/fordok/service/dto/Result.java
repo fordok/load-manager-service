ea19b337fb4bdbae05e9ca330b0632385bce1fa5
Rustam Tenishev <fordoki@gmail.com>
2015-10-17 19:52:24 +0300

    added Result fields

--- src/main/java/net/fordok/service/dto/Result.java
+++ src/main/java/net/fordok/service/dto/Result.java
@@ -1,7 +1,36 @@
 package net.fordok.service.dto;
 
+import java.util.Date;
+
 /**
  * Created by fordok on 10/9/2015.
  */
 public class Result {
+    private String resultId;
+    private Date startTs;
+    private Date stopTs;
+
+    public String getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(String resultId) {
+        this.resultId = resultId;
+    }
+
+    public Date getStartTs() {
+        return startTs;
+    }
+
+    public void setStartTs(Date startTs) {
+        this.startTs = startTs;
+    }
+
+    public Date getStopTs() {
+        return stopTs;
+    }
+
+    public void setStopTs(Date stopTs) {
+        this.stopTs = stopTs;
+    }
 }
