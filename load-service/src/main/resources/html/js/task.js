5e9bb27300a54d4f2aae48ca77de2405fd8d599d
Rustam Tenishev <fordoki@gmail.com>
2016-01-24 02:14:16 +0200

    addType JS function

--- load-service/src/main/resources/html/js/task.js
+++ load-service/src/main/resources/html/js/task.js
@@ -1,13 +1,12 @@
 var app = angular.module('taskApp', ['ngResource']);
 var taskController = app.controller('TaskController', function($http, $scope, $resource) {
 
-    $scope.alerts = [];
-
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
     };
 
     $scope.loadData = function() {
+        $scope.alerts = [];
         $http.get('http://localhost:8080/api/tasks').
             success(function(data) {
                 $scope.tasks = data;
@@ -25,6 +24,16 @@ var taskController = app.controller('TaskController', function($http, $scope, $r
             $scope.alerts.push({type: "success", msg: "Task was added"});
             $scope.loadData();
             $scope.task = {};
+        });
+
+    };
+
+    $scope.addType = function(){
+        var NewType = $resource('http://localhost:8080/api/types');
+        NewType.save($scope.type, function(type) {
+            $scope.alerts.push({type: "success", msg: "Type was added"});
+            $scope.loadData();
+            $scope.type = {};
         });
 
     };
